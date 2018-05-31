b8aa598c03b7e2a709a7db041be98529d502c7d8
matt smith and scott fraser <scott@>
2018-05-31 11:32:35 -0700

    make modal show input

--- js/marketscripts.js
+++ js/marketscripts.js
@@ -1,16 +1,33 @@
 
 
 $(document).ready(function(){
+  // $("#dropdown").submit(function(event) {
+  //   console.log(ready);
+  //   var spatChoice= $("select#spatula").val();
+  //   $(".spatInput").text(spatChoice);
+  // });
+
   $("#formOne").submit(function(event) {
-    var fullname= $("input#nameInput").val();
-    $(".nameInput").text(fullname);
+
+    var fullName= $("input#nameInput").val();
+    var streetName= $("input#streetInput").val();
+    var cityName= $("input#cityInput").val();
+    var zipName= $("input#zipInput").val();
+    var emailName= $("input#emailInput").val();
+
+    $(".nameInput").text(fullName);
+    $(".streetInput").text(streetName);
+    $(".cityInput").text(cityName);
+    $(".zipInput").text(zipName);
+    $(".emailInput").text(emailName);
+
     event.preventDefault();
     });
 
-    // $(".btn btn-primary").click(function(){
-    // $("#exampleModal").modal("hide");
+    $(".btn btn-primary").click(function(){
+    $("#exampleModal").modal("hide");
 
 
 
-  // });
+  });
 });
